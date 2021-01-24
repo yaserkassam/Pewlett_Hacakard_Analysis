032ed8c670bddaeeb0319825ac8c1b56c75a90ce
yaserkassam <yaser.m.kassam@gmail.com>
2021-01-24 12:07:59 -0500

    adding retirement CSV file

--- Analysis Projects/Pewlett_Hackard_Analysis/Queries/schema.sql
+++ Analysis Projects/Pewlett_Hackard_Analysis/Queries/schema.sql
@@ -1,10 +1,12 @@
 -- Creating tables for PH-EmployeeDB
+DROP TABLE departments CASCADE;
 CREATE TABLE departments (
      dept_no VARCHAR(4) NOT NULL,
      dept_name VARCHAR(40) NOT NULL,
      PRIMARY KEY (dept_no),
      UNIQUE (dept_name)
 );
+DROP TABLE employees CASCADE;
 CREATE TABLE employees(
 	 emp_no INT NOT NULL,
      birth_date DATE NOT NULL,
@@ -14,6 +16,7 @@ CREATE TABLE employees(
      hire_date DATE NOT NULL,
      PRIMARY KEY (emp_no)
 );
+DROP TABLE dept_manager CASCADE;
 CREATE TABLE dept_manager (
 	dept_no VARCHAR(4) NOT NULL,
     emp_no INT NOT NULL,
@@ -23,6 +26,7 @@ CREATE TABLE dept_manager (
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
     PRIMARY KEY (emp_no, dept_no)
 );
+DROP TABLE salaries CASCADE;
 CREATE TABLE salaries (
  	emp_no INT NOT NULL,
   	salary INT NOT NULL,
@@ -31,21 +35,43 @@ CREATE TABLE salaries (
   	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   	PRIMARY KEY (emp_no)
 );
-CREATE TABLE dept_emp(
+
+DROP TABLE dept_emp CASCADE;
+CREATE TABLE dept_emp (
 	emp_no INT NOT NULL,
-	dept_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	PRIMARY KEY (emp_no, dept_no)
 );
+drop table titles CASCADE;
 CREATE TABLE titles(
 	emp_no INT NOT NULL,
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
-	);
+	PRIMARY KEY (emp_no, title, from_date)
+);
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
-SELECT * FROM departments;
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+--Create new table
+DROP TABLE retirement_info
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+SELECT * FROM retirement_info;
